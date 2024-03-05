8541d73216cede42418caf25b1b31f934d94a301
NexusSoftTech29 <nexussofttech29@gmail.com>
2024-03-05 11:51:54 +0530

    React Native Project Template with ReactNavigation, NativeWind, Animations

---
+++ screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Hi There Logged In!</Text>
+    </View>
+  );
+}
